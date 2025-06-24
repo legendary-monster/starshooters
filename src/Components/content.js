a74e9763632b0ef856739ddba89b645d947c3065
legendary-monster <71312204+legendary-monster@users.noreply.github.com>
2025-06-24 13:24:53 +0530

    edited images and added gallery

--- src/Components/content.js
+++ src/Components/content.js
@@ -1,4 +1,4 @@
-import { Container, Carousel, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Carousel, Row, Col, Button } from 'react-bootstrap';
 
 const Content = () => {
     return (
@@ -6,27 +6,27 @@ const Content = () => {
             <Container>
                 <Carousel>
                     <Carousel.Item>
-                        <img src="/slide1.webp" alt="practice" text="First slide" />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
+                        <img src="images\dasarastateW.jpeg" alt="practice" text="First slide" />
+                        {/* <Carousel.Caption  style={{backgroundColor:"black",padding:'-5px -55rem', borderRadius:"50px"}}>
+                            <h3>Dasara State</h3>
+                            <p>Champions in the dasara district meet at 2024.</p>
+                        </Carousel.Caption> */}
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src="/slide2.webp" alt="practice" text="Second slide" />
-                        <Carousel.Caption>
+                        <img src="images\shankranthi2024.jpeg" alt="practice" text="Second slide" />
+                        {/* <Carousel.Caption>
                             <h3>Second slide label</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
+                        </Carousel.Caption> */}
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src="/slide3.webp" alt="practice" text="Third slide" />
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>
-                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                            </p>
-                        </Carousel.Caption>
+                        <img src="/images/hbg6.jpeg" alt="Girls Team" text="Third slide" />
+                        {/* <Carousel.Caption style={{backgroundColor:"black",padding:'-5px,5rem', borderRadius:"50px"}}>
+                            <h3>Dasara girls team</h3>
+                            <h5 >
+                                Girls team had secured 3rd place in the Dasara state tournament held at mysore
+                            </h5>
+                        </Carousel.Caption> */}
                     </Carousel.Item>
                 </Carousel>
             </Container>
@@ -35,7 +35,7 @@ const Content = () => {
                 <Row>
                     <Col md={4}>
                        <div className='Selva'>
-                        <img src="/selva.jpeg" alt="Selva" className="img-fluid coach-img" />
+                        <img src="/images/selva.jpeg" alt="Selva" className="img-fluid coach-img" />
                             <h3>Selva</h3>
                             <p>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
@@ -46,7 +46,7 @@ const Content = () => {
                     </Col>
                      <Col md={4}>
                         <div className='Prashanth'>
-                            <img src="/prashanth.jpeg" alt="prashanth" className="img-fluid coach-img"/>
+                            <img src="/images/prashanth.jpeg" alt="prashanth" className="img-fluid coach-img"/>
                             <h3>Prashanth</h3>
                             <p>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
@@ -57,7 +57,7 @@ const Content = () => {
                     </Col>
                      <Col md={4}>
                         <div className="Tilak">
-                            <img src="tilak.jpeg" alt="Tilak" className="img-fluid coach-img"/>
+                            <img src="/images/tilak.jpeg" alt="Tilak" className="img-fluid coach-img"/>
                             <h3>TILAK</h3>      
                             <p>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,19 +1,32 @@
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import { Container,Row,Col,Image } from 'react-bootstrap';
 
 const Gallery = () => {
     return (
         <>
-        <div className="gallery-page">
+        <div className="gallery-page" style={{height:"160vh"}}>
             <Header/>
+            <Container>
             <h1>Gallery Page</h1>
-            <p>This is the gallery page where you can view images.</p>
-            <div className="gallery-images">
-                <img src="#" alt="1" className="img-fluid" />
-                <img src="#" alt="2" className="img-fluid" />
-                <img src="#" alt="3" className="img-fluid" />
-            </div>
-            
+            <p>This is the gallery page our teams.</p>
+            <Row>
+                <Col>
+                <Image src="images/hbb1.jpeg" rounded height={"500rem"}width={"600rem"} />
+                </Col>
+              <Col>
+                <Image src="images/hbb2.jpeg" rounded height={"500rem"}width={"600rem"} />
+                </Col>
+            </Row>
+            <Row style={{marginTop:"2em"}}>
+                 <Col>
+                <Image src="images/hbg4.jpeg" rounded height={"500rem"}width={"600rem"} />
+                </Col>
+              <Col>
+                <Image src="images/hbg7.jpeg" rounded height={"500rem"}width={"600rem"} />
+                </Col>
+             </Row>   
+            </Container>
         </div>
         <Footer/>
         </>
