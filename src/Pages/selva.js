0038e1e073992f81f0c82982f2104b6f91cea595
legendary-monster <71312204+legendary-monster@users.noreply.github.com>
2025-07-04 15:35:24 +0530

    photo of coaches max width

--- src/Pages/selva.js
+++ src/Pages/selva.js
@@ -15,7 +15,6 @@ const Selva = () => {
             <h1 className="display-4 fw-bold mt-5">Selva</h1>
             <h3 className="text-muted">Secretary, Star Shooters Handball Club</h3>
             <p className="pdata">As the Secretary of this dynamic and passionate club, it gives me immense pride to share our journey and invite you into the energetic world of handball. Our club is more than just a team-it's a vibrant community built on teamwork, resilience, and an unshakable love for the sport.<br/><br/>
-
 Since our founding, we've been driven by one mission: to nurture talent, promote sportsmanship, and elevate the spirit of handball in our region and beyond. With a committed coaching staff, dedicated players, and an enthusiastic support base, we strive to create an environment that encourages growth-both on and off the court</p>
             </Col>
         </Row>
